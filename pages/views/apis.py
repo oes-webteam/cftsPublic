c1b3b73c083f15c8425311a2811f377d738b588d
Alex Alvarado <alexjalvarado66@gmail.com>
2021-06-29 08:09:09 -0400

    remove is_centcom from new user creation

--- pages/views/apis.py
+++ pages/views/apis.py
@@ -154,7 +154,7 @@ def process(request):
                 name_first=form_data.get('firstName'),
                 name_last=form_data.get('lastName'),
                 email=source_email,
-                is_centcom=form_data.get('isCentcom'),
+                #is_centcom=form_data.get('isCentcom'),
                 user_identifier=form_data.get('userID')
             )
             user.save()
