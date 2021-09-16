bfcf78819e982afe0787bd08245763dc839df7d4
Alex Alvarado <alexjalvarado66@gmail.com>
2021-09-16 09:18:52 -0400

    'visible' field added to Network and Rejection models, hide networks with no files in queue

--- pages/models.py
+++ pages/models.py
@@ -50,6 +50,7 @@ class Rejection(models.Model):
   name = models.CharField(max_length=50)
   subject = models.CharField(max_length=255)
   text = models.TextField()
+  visible = models.BooleanField(default=True)
 
   def __str__(self):
     return self.name
@@ -81,6 +82,7 @@ class Network(models.Model):
   name = models.CharField(max_length=50)
   classifications = models.ManyToManyField(Classification)
   sort_order = models.IntegerField()
+  visible = models.BooleanField(default=True)
 
   class Meta:
     ordering = ['sort_order']
