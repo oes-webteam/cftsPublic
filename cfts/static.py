db368eee1fbc6b8209c0749e59692e1026a22e55
Alex Alvarado <alexjalvarado66@gmail.com>
2021-11-26 08:09:22 -0500

    use ManifestStaticFileStoreage for collecting static files

---
+++ cfts/static.py
@@ -0,0 +1,11 @@
+from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
+
+class ManifestStaticFilesStorageSubClass(ManifestStaticFilesStorage):
+    # fix for certain css files throwing valueError when collecting static files
+    # source: https://code.djangoproject.com/ticket/21080#comment:12
+    patterns = (
+        ("*.css", (
+            r"""(url\((?:['"]|%22|%27){0,1}\s*(.*?)(?:['"]|%22|%27){0,1}\))""",
+            (r"""(@import\s*["']\s*(.*?)["'])""","""@import url("%s")"""),
+        )),
+    )
