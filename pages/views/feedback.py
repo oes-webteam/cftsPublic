b6eac3dba9be49f48cda4c75ac4cd37ee23f09f8
Alex <alexjalvarado66@gmail.com>
2022-01-27 11:27:13 -0500

    CFTS-561: feedback.py comments

--- pages/views/feedback.py
+++ pages/views/feedback.py
@@ -18,16 +18,16 @@ from pages.models import *
 from pages.views.auth import getCert, getOrCreateUser
 from django.contrib.auth.models import User as authUser
 
-
-import hashlib
-
-
+# function to serve feedback submission page with optional Request object
 def feedback(request, requestid=False):
     resources = ResourceLink.objects.all()
 
+    # get user cert info, get User object
     certInfo = getCert(request)
     cftsUser = getOrCreateUser(request, certInfo)
 
+    # if optional requestid passed in from args include it in the request context
+    # requestid is only passed in from a ban request and is used to link to the Request object that warrented a user ban
     if requestid != False:
         rc = {'resources': resources, 'user': cftsUser, 'rqst': Request.objects.get(request_id=requestid)}
     else:
@@ -36,14 +36,18 @@ def feedback(request, requestid=False):
     return render(request, 'pages/feedback.html', {'rc': rc})
 
 
+# function to create Feedback object
 def submitFeedback(request):
 
+    # only process data from a POST request
     if request.method == 'POST':
         form_data = request.POST
 
+        # get user cert info, get User object
         certInfo = getCert(request)
         cftsUser = getOrCreateUser(request, certInfo)
 
+        # create Feedback object from POST data
         feedback = Feedback(
             title=form_data.get('title'),
             body=form_data.get('feedback'),
@@ -52,9 +56,11 @@ def submitFeedback(request):
             date_submitted=timezone.now()
         )
 
+        # if a User object was returned then create a one-to-one realationship between User and Feedback objects
+        # submitting feedback does not require a user to be logged in so external users will always return None
         if cftsUser != None:
             feedback.user = cftsUser
-
+        # no User object retruned, format and add user submitted information to the beginning of the body of the Feedback object
         else:
             buggedUserInfo = '''
             User Name: {uname}
@@ -64,15 +70,16 @@ def submitFeedback(request):
             Phone: {phone}
 
             '''.format(uname=form_data.get('userName'), fname=form_data.get('firstName'), lname=form_data.get('lastName'), email=form_data.get('userEmail'), phone=form_data.get('userPhone'))
+
             feedback.body = buggedUserInfo + form_data.get('feedback')
 
-            # bugged PKI user, try and return a CFTS userser account based on username
+            # bugged PKI user, try and return a User object based on Django account username
             try:
                 userFromUserName = User.objects.get(auth_user=authUser.objects.get(username=form_data.get('userName')))
                 feedback.user = userFromUserName
             # no luck with username
             except (User.DoesNotExist, authUser.DoesNotExist):
-                # try thier emmail
+                # try their email
                 try:
                     userFromEmail = User.objects.get(source_email=Email.objects.get(address=form_data.get('userEmail')))
                     feedback.user = userFromEmail
