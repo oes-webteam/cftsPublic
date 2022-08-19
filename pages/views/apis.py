9dc11aa59f59203cc85a4314b7b9968b1b5ceea1
Alex <55999674+Alexander-Alvarado@users.noreply.github.com>
2022-08-19 14:32:30 -0400

    Xanders_last_update (#150)

--- pages/views/apis.py
+++ pages/views/apis.py
@@ -192,9 +192,10 @@ def createEml(request, request_id, files_list, reasons):
     # Create a mailto link...
     # Yeah, that's how we send out system emails because we aren't allowed to have an email relay server... thanks J6
     if EMAIL_HOST == '':
-        msgBody = "mailto:" + str(rqst.user.source_email) + "?cc=" + str(rqst.RHR_email) + "&subject=[" + EMAIL_CLASSIFICATION + "] CFTS File Rejection&body=Classification: " + EMAIL_CLASSIFICATION + "%0D%0A%0D%0AThe following files have been rejected from your transfer request:%0D%0A"
+        msgBody = "mailto:" + str(rqst.user.source_email) + "?cc=" + str(rqst.RHR_email) + \
+            "&subject=[" + EMAIL_CLASSIFICATION + "] CFTS File Rejection&body=Classification: " + EMAIL_CLASSIFICATION + "%0D%0A%0D%0AThe following files have been rejected from your transfer request:%0D%0A"
     else:
-        msgBody = "Classification: " + EMAIL_CLASSIFICATION +"\n\nThe following files have been rejected from your transfer request:\n        "
+        msgBody = "Classification: " + EMAIL_CLASSIFICATION + "\n\nThe following files have been rejected from your transfer request:\n        "
 
     # List the names of all the files being rejected in the email
     files = File.objects.filter(file_id__in=files_list)
@@ -225,16 +226,27 @@ def createEml(request, request_id, files_list, reasons):
                                                                                         'url': url, 'comments': rejectionComments}, request)
 
     if EMAIL_HOST != '':
-        email = EmailMessage(
-            '[' + EMAIL_CLASSIFICATION + '] CFTS File Rejection',
-            msgBody,
-            "Combined File Transfer Service <" + EMAIL_FROM_ADDRESS + ">",
-            [str(rqst.user.source_email), ],
-            cc=[str(rqst.RHR_email), ],
-            reply_to=[IM_ORGBOX_EMAIL, ],
-        )
+        try:
+            email = EmailMessage(
+                '[' + EMAIL_CLASSIFICATION + '] CFTS File Rejection',
+                msgBody,
+                "Combined File Transfer Service <" + EMAIL_FROM_ADDRESS + ">",
+                [str(rqst.user.source_email), ],
+                cc=[str(rqst.RHR_email), ],
+                reply_to=[IM_ORGBOX_EMAIL, ],
+            )
 
-        email.send(fail_silently=False)
+            email.send(fail_silently=False)
+        except:
+            email = EmailMessage(
+                '[' + EMAIL_CLASSIFICATION + '] CFTS File Rejection',
+                msgBody,
+                "Combined File Transfer Service <" + EMAIL_FROM_ADDRESS + ">",
+                [str(rqst.user.source_email), ],
+                reply_to=[IM_ORGBOX_EMAIL, ],
+            )
+
+            email.send(fail_silently=False)
 
     return msgBody
 
