2d22e17570d74863f02f76acc112da155289c5db
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2025-02-14 11:16:53 -0500

    Review in progress/complete

--- cfts/static/js/review-progress.js
+++ cfts/static/js/review-progress.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Add checkmarks based on review status
     document.querySelectorAll(".file-row").forEach(row => {
-        let fileId = row.id.split("_")[1];  // Extract file_id from row ID
         let firstReviewerCompleted = row.getAttribute("data-first-reviewer") === "true";
         let secondReviewerCompleted = row.getAttribute("data-second-reviewer") === "true";
+        let reviewerBegin = document.getElementById('btn-review')
         
         let checkmarkContainer = document.createElement("span");
 
@@ -19,13 +19,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     const showMoreBtn = document.getElementById("showMoreBtn");
+    const extraFiles = document.querySelectorAll(".extra-files"); // Select all hidden files
 
     if (showMoreBtn) {
         showMoreBtn.addEventListener("click", function () {
-            let extraFiles = document.querySelectorAll(".extra-files");
-
             extraFiles.forEach(file => file.classList.toggle("d-none"));
 
+            // Toggle button text
             if (showMoreBtn.innerText.includes("Show")) {
                 showMoreBtn.innerText = "Show Less";
             } else {
